db2c3cdf1e08ae810b89f8f0b58fa7eff4ff3edc
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-02-09 19:37:12 -0500

    Setup firebase project info.

---
+++ headstarter-connect-app/src/config/firebaseconfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpzhzNbQH5fKesJpRBS8PhIMByn2RXRB4",
+  authDomain: "headstarter-connect.firebaseapp.com",
+  projectId: "headstarter-connect",
+  storageBucket: "headstarter-connect.appspot.com",
+  messagingSenderId: "518236808134",
+  appId: "1:518236808134:web:49f343b62560a999cb48c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// export for use in React
+export const auth = getAuth(app);
+export const db = getFirestore(app);
