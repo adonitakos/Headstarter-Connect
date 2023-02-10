0afd8db7cba62ebcee18a476cf1ff171bb0be39c
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-02-10 16:35:28 -0500

    Implemented Basic Registration

--- headstarter-connect-app/src/pages/SignUp.jsx
+++ headstarter-connect-app/src/pages/SignUp.jsx
@@ -1,20 +1,55 @@
 // File: /src/pages/SignUp.jsx
 
-import React from "react";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { auth, db } from '../config/firebaseconfig'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
 import { AuthContainer, AuthBanner, AuthInput, AuthButton } from "../components/AuthElements";
+import { Alert, Form } from 'react-bootstrap'
 
 function SignUp() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const navigate = useNavigate()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(async ({ user }) => {
+            const userInfo = {
+            id: user.uid,
+            email: user.email
+            }
+            await setDoc(doc(db, 'users', user.uid), userInfo)
+            return userInfo
+        })
+        .then(user => {
+                navigate(`/profile`)
+            }
+        )
+        .catch(error => {
+            console.error('An error occurred while creating an account: ', error)
+            setErrorMessage(error.message)
+        })
+    }
     return (
-        <>
+        <Form onSubmit={handleSubmit}>
+            {errorMessage !== '' && <Alert variant="danger">{errorMessage}</Alert>}
             <AuthContainer>
                 <AuthBanner>Sign Up</AuthBanner>
-                <AuthInput type="text" placeholder="First Name" style={{ width: "200px" }} />
-                <AuthInput type="text" placeholder="Last Name" style={{ width: "200px" }} />
-                <AuthInput type="text" placeholder="Email Address" style={{ width: "410px" }} />
-                <AuthInput type="password" placeholder="Password" style={{ width: "410px" }} />       
-                <AuthButton>Register</AuthButton>         
+                <Form.Group className="my-2">
+                    <AuthInput type="text" placeholder="Email Address" onChange={(e) => { setEmail(e.target.value) }} style={{ width: "410px" }} />
+                </Form.Group>
+                <Form.Group className="my-2">
+                    <AuthInput type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} style={{ width: "410px" }} />       
+                </Form.Group>
+                <Form.Group className="my-3">
+                    <AuthButton type="submit">Register</AuthButton>
+                </Form.Group>         
             </AuthContainer>
-        </>
+        </Form>
     );
 } // <--- SignUp() fucntion ends here
 
