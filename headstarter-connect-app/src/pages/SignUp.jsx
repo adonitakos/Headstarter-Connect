566c4d15ef76e1621dad489e547376a1411dd831
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-02-10 17:29:19 -0500

    Added Registration fields + user must fill in all fields.

--- headstarter-connect-app/src/pages/SignUp.jsx
+++ headstarter-connect-app/src/pages/SignUp.jsx
@@ -11,6 +11,9 @@ import { Alert, Form } from 'react-bootstrap'
 function SignUp() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [squidNum, setSquidNum] = useState(0)
+    const [groupName, setGroupName] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
     const navigate = useNavigate()
 
@@ -18,17 +21,19 @@ function SignUp() {
         e.preventDefault()
         createUserWithEmailAndPassword(auth, email, password)
         .then(async ({ user }) => {
-            const userInfo = {
-            id: user.uid,
-            email: user.email
+                const userInfo = {
+                id: user.uid,
+                email: user.email,
+                name: name,
+                squidNum: squidNum,
+                groupName: groupName
             }
             await setDoc(doc(db, 'users', user.uid), userInfo)
             return userInfo
         })
         .then(user => {
-                navigate(`/profile`)
-            }
-        )
+            navigate(`/profile`)
+        })
         .catch(error => {
             console.error('An error occurred while creating an account: ', error)
             setErrorMessage(error.message)
@@ -39,13 +44,22 @@ function SignUp() {
             {errorMessage !== '' && <Alert variant="danger">{errorMessage}</Alert>}
             <AuthContainer>
                 <AuthBanner>Sign Up</AuthBanner>
-                <Form.Group className="my-2">
-                    <AuthInput type="text" placeholder="Email Address" onChange={(e) => { setEmail(e.target.value) }} style={{ width: "410px" }} />
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
+                    <AuthInput type="text" placeholder="Email Address" onChange={(e) => { setEmail(e.target.value) }} style={{ width: "410px" }} required />
                 </Form.Group>
-                <Form.Group className="my-2">
-                    <AuthInput type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} style={{ width: "410px" }} />       
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
+                    <AuthInput type="text" placeholder="Full Name" onChange={(e) => { setName(e.target.value) }} style={{ width: "410px" }} required />
                 </Form.Group>
-                <Form.Group className="my-3">
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
+                    <AuthInput type="text" placeholder="Group" onChange={(e) => { setGroupName(e.target.value) }} style={{ width: "410px" }} required />
+                </Form.Group>
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
+                    <AuthInput type="number" placeholder="Squid #" onChange={(e) => { setSquidNum(Number(e.target.value)) }} style={{ width: "410px" }} required />
+                </Form.Group>
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
+                    <AuthInput type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} style={{ width: "410px" }} required />       
+                </Form.Group>
+                <Form.Group className="mx-auto d-flex justify-content-center mb-2">
                     <AuthButton type="submit">Register</AuthButton>
                 </Form.Group>         
             </AuthContainer>
