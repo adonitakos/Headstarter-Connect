3c9b0b175fc37b008b4142f63d44778259930824
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-02-16 00:00:11 -0500

    Calendar clicks on /profile successfully POST to Firestore. Moved bottom portion of / to /profile.

--- headstarter-connect-app/src/pages/Calendar.jsx
+++ headstarter-connect-app/src/pages/Calendar.jsx
@@ -1,34 +1,55 @@
 // File: /src/pages/Calendar.jsx
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import Calendar from 'react-calendar';
 import { useNavigate } from 'react-router-dom';
-import { useUser } from '../config/user';
+import { useUser, UserContext } from '../config/user';
+import { doc, getDoc, setDoc, onSnapshot } from 'firebase/firestore'
+import { db } from '../config/firebaseconfig'
+import { overlapAvabls } from '../config/utils'
 
 function CalendarPage() {
+  const userID=useContext(UserContext)[0].user.id
+  let availInfo=useContext(UserContext)[2]
+  if(availInfo.length===0)
+    availInfo=[{}]        
+  let meetInfo={}
+  if(availInfo.length>=1)
+    meetInfo=overlapAvabls(availInfo)
 
   const [state, team] = useUser();
   const navigate = useNavigate();
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState({});
 
-// Make it so only logged in users can access this page
-  if (!state.user) {
-    navigate('/auth/login'); // Redirect the user to the login page
-    return null; // Don't render anything
+  useEffect(() => {
+    // Make it so only logged in users can access this page
+    if (!state.user) {
+      navigate('/auth/login'); // Redirect the user to the login page
+      return null; // Don't render anything
+    }
+    // otherwise, update the current availability
+    if(userID!==undefined && Object.keys(notes).length !== 0)
+      setDoc(doc(db, 'users', userID), {"availability":notes},{merge: true})
+  })
+
+  function clearSchdl() {
+    setDoc(doc(db, 'users', userID), {"availability":{}},{merge: true})
   }
 
   function handleDateClick(clickedDate) {
     setDate(clickedDate);
     const note = window.prompt(`Enter note for ${clickedDate.toDateString()}`, '', 'background-color: #ffffff; border: 1px solid #333');
+    if(userID!==undefined)
+      getDoc(doc(db, 'users', userID)).then((doc) => console.log(doc.get('availability'))).catch((err) => {console.log(err)})
     if (note) {
-      setNotes({ ...notes, [clickedDate.toDateString()]: note });
-    }
+      setNotes({ ...notes, [(clickedDate.getMonth()+1).toString().padStart(2, "0")+"_"+(clickedDate.getDate()).toString().padStart(2, "0")+"_"+String(clickedDate.getFullYear())]: note });
+    };
   } // <--- handleDateClick() function ends here
 
   return (
-    <div className="d-flex h-100" style={{fontFamily:'Poppins, sans-serif', marginTop:'-12rem'}}>
-      <div className="align-self-center mx-auto">
+    <div style={{fontFamily:'Poppins, sans-serif'}}>
+      <div className="h-100 align-self-center mx-auto mb-2">
         <h1 className="display-4 text-primary text-center mb-4" style={{fontWeight:'700'}}>Team Calendar</h1>
         <Calendar
           onClickDay={handleDateClick}
@@ -42,6 +63,14 @@ function CalendarPage() {
             Date written: {date.toDateString()}
           </p>
         )}
+      </div>
+      <button className="mx-auto d-flex justify-content-center" onClick={clearSchdl}>Reset</button>
+      <br></br>
+      <div>
+        <p className="h5 text-primary rounded mx-auto d-flex justify-content-center">MEMBER AVAILABILITIES</p>
+        <p className="col-md-5 h5 rounded mx-auto d-block">{JSON.stringify(availInfo, null, 2)}</p>
+        <p className="h5 text-primary rounded mx-auto d-flex justify-content-center">MEETING TIMES</p>
+        <p className="col-md-5 h5 rounded mx-auto d-block">{JSON.stringify(meetInfo, null, 2)}</p>
       </div>
     </div>
   );
--- headstarter-connect-app/src/pages/Profile.jsx
+++ headstarter-connect-app/src/pages/Profile.jsx
@@ -3,20 +3,14 @@
 import Card from 'react-bootstrap/Card'
 import React, { useContext } from "react";
 import { UserContext } from '../config/user'
-import { overlapAvabls } from '../config/utils'
 
 function Profile() {
     let userInfo=useContext(UserContext)[0].user
     let teamInfo=useContext(UserContext)[1]
-    let availInfo=useContext(UserContext)[2]
-    if(userInfo===null || teamInfo===null || availInfo.length===0) {
+    if(userInfo===null || teamInfo===null) {
         userInfo={"name":"","squidNum":"","groupName":""}
-        teamInfo=""
-        availInfo=[{}]        
+        teamInfo=""       
     }
-    let meetInfo={}
-    if(availInfo.length>=1)
-        meetInfo=overlapAvabls(availInfo)
     return (
         <div className="container">
             <div>
@@ -37,10 +31,6 @@ function Profile() {
                     <p className="col-md-3 rounded mx-auto d-block"><b>Teammates</b>: {teamInfo}</p>
                 </div>
             </div>
-            <p className="h5 text-primary rounded mx-auto d-flex justify-content-center">MEMBER AVAILABILITIES</p>
-            <p className="col-md-5 h5 rounded mx-auto d-block">{JSON.stringify(availInfo, null, 2)}</p>
-            <p className="h5 text-primary rounded mx-auto d-flex justify-content-center">MEETING TIMES</p>
-            <p className="col-md-5 h5 rounded mx-auto d-block">{JSON.stringify(meetInfo, null, 2)}</p>
         </div>
     );
 
